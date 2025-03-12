d7b3cc125d3b752a87683547525882564b73b6d0
adroit-nick <abrortoshaliyev@gmail.com>
2025-03-12 22:25:48 +0900

    fix3

--- apps/nestar-api/src/schemas/Member.model.ts
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -1,5 +1,5 @@
 import { Schema } from 'mongoose';
-import { MemberType, MemberStatus, MemberAuthType } from './member.enum';
+import { MemberAuthType, MemberStatus, MemberType } from '../libs/enums/member.enum';
 
 const MemberSchema = new Schema(
  {
