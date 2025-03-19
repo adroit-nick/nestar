4b8656f2370dc514a649a26ea20f29fa3ee0b5e9
adroit-nick <abrortoshaliyev@gmail.com>
2025-03-19 21:42:15 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Resolver()
 export class MemberResolver {
@@ -31,10 +34,29 @@ export class MemberResolver {
     @Mutation(() => String)
     public async updateMember(@AuthMember('_id') memberId: ObjectId ): Promise<string> {
         console.log('Mutation: updateMember');
-        console.log(typeof memberId);
-        console.log(memberId);
         return this.MemberService.updateMember();
     }
+
+    @UseGuards(AuthGuard)
+    @Query(() => String)
+    public async checkAuth(@AuthMember('memberNick') memberNick : string ): Promise<string> {
+        console.log('Query: checkAuth');
+        console.log('memberNick: memberNick');
+        return `Hi ${memberNick}`;
+    }
+
+    @Roles(MemberType.USER, MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query(() => String)
+    public async checkAuthRoles(@AuthMember() authMember : Member ): Promise<string> {
+        console.log('Query: checkAuthRoles');
+        return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
+    }
+
+
+
+
+
 
     @Query(() => String)
     public async getMember(): Promise<string> {
@@ -44,6 +66,8 @@ export class MemberResolver {
 
     /** ADMIN **/
     //Authorization: ADMIN
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
     @Mutation(() => String)
 public async getAllMembersByAdmin(): Promise<string> {
     return this.MemberService.getAllMembersByAdmin();
