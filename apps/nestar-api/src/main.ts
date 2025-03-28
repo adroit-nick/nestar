98359ba2b87a31b436efcd808d1f0e3225c66749
adroit-nick <abrortoshaliyev@gmail.com>
2025-03-28 18:05:40 +0900

    fix bugs final

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,13 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './libs/types/interceptor/Logging.interceptor';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(process.env.PORT_API ?? 3000);
+import {ObjectId} from 'bson';
+
+export const availableAgentSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews", "memberRank" ];
+
+export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberViews" ];
+
+ // IMAGE CONFIGURATION (config.js)
+ import { v4 as uuidv4 } from 'uuid';
+ import * as path from 'path';
+
+ export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+ export const getSerialForImage = (filename: string) => {
+   const ext = path.parse(filename).ext;
+   return uuidv4() + ext;
+ };
+
+export const shapeIntoMongoObjectId = (target: any) => {
+ return typeof target === 'string' ? new ObjectId(target): target;
 }
-bootstrap();
- 
